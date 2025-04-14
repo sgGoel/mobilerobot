1c474638394f46dcba15b77f55af2f22e62ad054
Layla <layladawit7@gmail.com>
2025-04-14 19:05:55 -0400

    Connecting serial monitor

--- src/robot/robot.cpp
+++ src/robot/robot.cpp
@@ -10,7 +10,12 @@ void setup() {
   delay(1000);
   initReceiver();
 
+  // ADDED
+  Serial.begin(9600);
+  while (!Serial); // wait for serial port to connect
+  Serial.println("Ready for input:");
 }
+
 
 void loop() {
   //readJoysticks();
@@ -20,4 +25,28 @@ void loop() {
   //sendData(); // Sends data using ESP-NOW to reciever
   printData();  // Prints data via serial port
   delay(20);
+
+  // ADDED
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    Serial.print("Input: ");
+    Serial.println(input);
+    if (input == "Forward" or input == "forward"){
+      //Forward command
+      
+    }
+    else if (input == "Back" or input == "back"){
+      //Back command
+    }
+    else if (input == "Left" or input == "left"){
+      //Left command
+    }
+    else if (input == "Right" or input == "right"){
+      //Right command
+    }
+    else if (input == "Stop" or input == "stop"){
+      //Stop command
+    }
+  }
+
 }
